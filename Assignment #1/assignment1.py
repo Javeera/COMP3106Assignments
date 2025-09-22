3463dee093ce4ebad2e3433c85a202246537dfd8
Javeera <javeerafaizi@gmail.com>
2025-09-22 19:27:45 -0400

    storing all labels in the grid

--- Assignment #1/assignment1.py
+++ Assignment #1/assignment1.py
@@ -16,6 +16,25 @@ def pathfinding(filepath):
       grid.append(row)
   # grid contains all the labels from the csv file (X, O, 1-9, S, G)
 
+  #get all the labels (X, O, 1-9, S, G)
+  goals = []
+  walls = set()
+  treasures = {}
+  rows = len(grid)
+  cols = len(grid[0]) 
+
+  #loop through each coordinate of the grid
+  for i in range(rows):
+    for j in range(cols):
+      if grid[i][j] == 'S':
+        start = (i, j)
+      elif grid[i][j] == 'G':
+        goals.append((i, j))
+      elif grid[i][j] == 'X':
+        walls.add((i, j))
+      elif grid[i][j].isdigit():
+        treasures[(i, j)] = int(grid[i][j])
+
   # optimal_path is a list of coordinate of squares visited (in order)
   # optimal_path_cost is the cost of the optimal path
   # num_states_explored is the number of states explored during A* search
