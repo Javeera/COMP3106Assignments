de54f977855058be427109c24a30dfc1e241a58e
gbassali <gurleenbassali@gmail.com>
2025-11-07 14:04:39 -0500

    add comments + fix current code errors

--- Assignment #3/assignment3.py
+++ Assignment #3/assignment3.py
@@ -15,33 +15,33 @@ class td_qlearning:
 
   def __init__(self, directory):
     # directory is the path to a directory containing trials through state space
-    # Return nothing
-    self.q = {} #q table (state, action): value
+    # Constructor --> Returns nothing
+    self.Q = {} #q table (state, action): value
     self.rewards = {} #state rewards
 
     #load all trials from the directory
-    trails = []
+    trials = []
     for file in os.listdir(directory):
       if file.endswith(".csv"):
         filepath = os.path.join(directory, file)
         df = pd.read_csv(filepath, header=None)
-        trails.append(df)
+        trials.append(df)
 
-    #Q(s,a) = r(s)
-    #gets the state and action and initializes the q table with rewards
-    for trial in trails:
-      for row in trial:
-        if len(row) < 2:
-          continue
-        state = str(row[0]).strip()
-        action = int(row[1])
-        reward = self.rewards(state)
-        self.q[(state,action)] = reward
+    #gets all states and actions and initializes the q table with rewards
+    for trial in trials:
+      for s, a in trial.values:
+        state = str(s).strip()
+        action = str(a)
+        reward = self.reward(state)
+        self.Q[(state, action)] = reward # initially estimate Q(s,a) = r(s) for all state-action pairs observed in the trials
+
+    print(self.Q)
+    print(self.rewards)
 
     #TODO: Run Q-learning until convergence
     converged = False
       #go thru each trial (state, action)
-      for trial in trials:
+      #for trial in trials:
       #extract current state and action and next state
       
       #compute immediet rewards for next state
@@ -49,41 +49,41 @@ class td_qlearning:
       #if yes valid then update q value else use reward of next state
       #td
 
-
-  def rewards(self, state):
-    #return the reward for a given state
+  #return the reward for a given state
+  def reward(self, state):
     if state in self.rewards: #i.e. if we already calculated the reward for this state
       return self.rewards[state]
     try:
       c_bag = int(state.split('/')[0])
       c_agent = int(state.split('/')[1])
       c_opponent = int(state.split('/')[2])
-      winner = int(state.split('/')[3])
+      winner = state.split('/')[3]
     except:
       return 0
     
-    if winner == 'A':
-      reward = c_agent #return reward
-    elif winner == 'O':
-      reward = -c_agent #return negative reward
-    else:
-      reward = 0
+    if winner == 'A':   # agent wins
+      reward = c_agent  # return reward
+    elif winner == 'O': # opponent wins
+      reward = -c_agent # return negative reward
+    else:               # non terminal state
+      reward = 0        # no reward
     
     self.rewards[state] = reward
     return reward
   
+  # Return a list of integers representing the available actions in the given state
   def available_actions(self, state):
-    # Return a list of integers representing the available actions in the given state
     try:
-      c_bag = int(state.split('/')[0])
+      c_bag = int(state.split('/')[0]) # Remaining coins in the bag
     except:
       return []
 
     if c_bag <= 0:
       return []
-    return [i for i in [1, 2, 3] if i <= c_bag]
+    # Available actions are 1, 2, or 3 coins --> cannot exceed remaining coins in the bag
+    return [i for i in [1, 2, 3] if i <= c_bag] 
 
-  # implementing qvalue function
+  # Qvalue function
   def qvalue(self, state, action):
     # state is a string representation of a state
     # action is an integer representation of an action
@@ -92,11 +92,14 @@ class td_qlearning:
     return round(self.Q[state][action], 2)
 
   # implementing the policy function
-  def policy(self, state):
-    # state is a string representation of a state
+  # def policy(self, state):
+  #   # state is a string representation of a state
 
-    # Return the optimal action (as an integer) under the learned policy
-    if not self.Q[state]:
-      return 1
-    else:
-      return max(self.Q[state], key=self.Q[state].get)
+  #   # Return the optimal action (as an integer) under the learned policy
+  #   if not self.Q[state]:
+  #     return 1
+  #   else:
+  #     return max(self.Q[state], key=self.Q[state].get)
+
+dir_path = "Examples/Example0/Trials"
+agent = td_qlearning(dir_path)
